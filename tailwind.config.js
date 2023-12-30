2d01c10b04eb0944162f7e897f8f85526a27b8a0
reZerOR <gkraihan14k@gmail.com>
2023-12-30 19:39:17 +0600

    project based custom color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      color : {
+        "backgroundprime": '#EFF0EA',
+        "textPrime": '#1D2424',
+        "prime": '#21857B',
+        "secondPrime": '#C1F1EC',
+        "textSecondery": "#4C5151"
+      }
+    },
   },
   plugins: [],
 }
